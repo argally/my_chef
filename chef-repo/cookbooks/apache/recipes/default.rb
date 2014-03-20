ed745bfce6b902a1030000846ae667436e5eb787
argally <amandargalligan@gmail.com>
2014-03-20 19:52:16 +0000

    Chef apache template update

--- chef-repo/cookbooks/apache/recipes/default.rb
+++ chef-repo/cookbooks/apache/recipes/default.rb
@@ -6,18 +6,38 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-# install apache
-package "apache2" do 
+
+# install apache but make it run on centos or ubuntu 
+
+
+package_name = "apache2"
+service_name = "apache2"
+document_root = "/var/www"
+
+if node["platform"] == "centos"
+	package_name == "httpd"
+	service_name == "httpd"
+	document_root == "/var/www/html"
+end
+
+package package_name do 
 	action :install
 end
 
 #install apache service
-service "apache2" do
+service service_name do
 	action [:start, :enable]
 end
 
 #write the homepage
-cookbook_file "/var/www/index.html" do
-	source "index.html"
+#cookbook_file "#{document_root}/index.html" do
+#	source "index.erb"
+#	mode "0644"
+#end
+
+
+#Define an erb template 
+template "#{document_root}/index.html" do
+	source "index.html.erb"
 	mode "0644"
 end
